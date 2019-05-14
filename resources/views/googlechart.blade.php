1c33b93df94cf5f402917dc0e80d56db062094b9
steph1902 <stephanus1902@gmail.com>
2019-05-14 19:12:21 +0700

    finish some ui
    finish some ui

--- resources/views/googlechart.blade.php
+++ resources/views/googlechart.blade.php
@@ -1,5 +1,27 @@
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
+  
+  <style>
+    .icon {
+      width: 16px;
+      height: 16px;
+    }
+  </style>
+  
   <!--Load the AJAX API-->
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script type="text/javascript">
@@ -84,14 +106,6 @@
       function drawChart3()
       {
 
-        //date,visitors,pageviews
-
-        // 'dailyVisitDate','dailyVisitors','dailyPageView'));
-
-        // Create the data table.
-        // var gagdet = {!! json_encode($gagdet) !!};
-        // var visitors = {!! json_encode($getVisitorByGagdet) !!};
-        // var record = {!! json_encode($getVisitorByGagdet) !!};
 
         var dailyVisitDate = {!! json_encode($dailyVisitDate) !!};
         var dailyVisitors = {!!json_encode($dailyVisitors) !!};
@@ -120,7 +134,7 @@
           'width':800,
           'height':300,
           'curveType': 'function',
-          };
+        };
 
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.LineChart(document.getElementById('chart_div3'));
@@ -132,18 +146,73 @@
   </head>
 
   <body>
-    <!--Table and divs that hold the pie charts-->
-    <table class="columns">
-      <tr>
-        <td><div id="chart_div" style="border: 1px solid #ccc"></div></td>
-        <td><div id="chart_div2" style="border: 1px solid #ccc"></div></td>
-      </tr>
-      <tr>
-        <td colspan="2"><div id="chart_div3" style="border: 1px solid #ccc"></div></td>
-      </tr>
-    </table>
-    <!--Div that will hold the pie chart-->
-    <!-- <div id="chart_div"></div>
-      <div id="chart_div2"></div> -->
-    </body>
-    </html>
+
+
+    <nav class="navbar navbar-light bg-light justify-content-between">
+      <a class="navbar-brand">QR Code - Edit URL Page</a>
+
+      <form class="form-inline">
+        <!-- <input class="form-control <main></main>r-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+
+        <a href=" {{ url('changepasswordpage') }}">
+          <img src="{{ asset('svg/people.svg') }}" alt="icon name" class="icon float-right">
+        </a>
+        <div>&nbsp &nbsp &nbsp &nbsp &nbsp</div>
+
+        <a href="{{ url('logout') }}">
+          <img src="{{ asset('svg/account-logout.svg') }}" alt="icon name" class="icon float-right">
+        </a>
+
+        <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+      </form>
+    </nav>
+
+   
+
+    
+   
+  <div class="container-fluid">
+      <div class="container-fluid">
+        <div class="card-body"> 
+          Range Waktu
+          <table class="table">
+            <tr>
+              <td><input type="date"></td>
+              <td><input type="date"></td>
+              <td><button type="submit" class="btn btn-primary float-right">Apply</button></td>
+              <td> 
+                <a href=""> 
+                  <img src="{{ asset('svg/data-transfer-download.svg') }}" alt="icon name" class="icon"> 
+                </a>
+              </td>
+              <td>&nbsp</td><td>&nbsp</td><td>&nbsp</td><td>&nbsp</td><td>&nbsp</td><td>&nbsp</td><td>&nbsp</td>            
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+
+     <div class="container-fluid">
+      <div class="container-fluid">
+        <div class="card-body">
+         <!--Table and divs that hold the pie charts-->
+         <table class="columns">
+          <tr>
+            <td><div id="chart_div" style="border: 1px solid #ccc"></div></td>
+            <td><div id="chart_div2" style="border: 1px solid #ccc"></div></td>
+          </tr>
+          <tr>
+            <td colspan="2"><div id="chart_div3" style="border: 1px solid #ccc"></div></td>
+          </tr>
+        </table>
+        
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+</body>
+</html>
